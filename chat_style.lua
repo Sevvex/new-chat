ab4a03887ae675f5e5836c93341889444af965a4
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-10-02 21:45:59 +0200

    Update chat_style.lua

--- chat_style.lua
+++ chat_style.lua
@@ -1,8 +1,9 @@
 AddEventHandler('chatMessage', function(source, name, message)
     CancelEvent()
-    local styledMessage = string.format("<font color='cyan'>%s</font>: %s", name, message)
-    TriggerClientEvent('chat:addMessage', -1, {
-        args = { styledMessage },
-        color = { 255, 255, 255 }
-    })
+    local styledMessage = {
+        type = "chatMessage",
+        username = name,
+        message = message
+    }
+    TriggerClientEvent('chat:addMessage', -1, styledMessage)
 end)
